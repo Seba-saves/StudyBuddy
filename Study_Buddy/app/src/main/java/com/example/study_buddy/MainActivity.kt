f195a3ddac05d3db0a0ba15df05058bfaa0d1f89
Seba-saves <5301843@gmail.com>
2023-03-25 21:48:33 -0500

    Character selected screen

--- Study_Buddy/app/src/main/java/com/example/study_buddy/MainActivity.kt
+++ Study_Buddy/app/src/main/java/com/example/study_buddy/MainActivity.kt
@@ -2,11 +2,21 @@ package com.example.study_buddy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+/*
+        val Name = findViewById<EditText>(R.id.editText)
+        val submitButton = findViewById<Button>(R.id.submitButton)
+        submitButton.setOnClickListener{
+            display
+        }
+
+ */
     }
 }
