20271fb07173228f9aa3a5da523e6f0d0b815f4b
Lau-m <laura.morales@my.utsa.edu>
2023-03-26 04:47:46 -0500

    new overrides to support menu

--- Study_Buddy/app/src/main/java/com/example/study_buddy/MainActivity.kt
+++ Study_Buddy/app/src/main/java/com/example/study_buddy/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.study_buddy
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
 import android.view.View
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
@@ -13,8 +15,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
+    }
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.main_menu, menu)
+        return true
     }
 
     fun sentData(view: View){
