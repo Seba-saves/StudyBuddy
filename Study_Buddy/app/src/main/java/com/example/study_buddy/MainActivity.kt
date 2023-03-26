e7ff6cd0896cf22ebd2155a9e63c8ce12948754d
Lau-m <laura.morales@my.utsa.edu>
2023-03-26 01:42:05 -0500

    Home screen with character and nav bar toggle

--- Study_Buddy/app/src/main/java/com/example/study_buddy/MainActivity.kt
+++ Study_Buddy/app/src/main/java/com/example/study_buddy/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
 
 class MainActivity : AppCompatActivity() {
     private var isMainLayout = true
